12f828847f3bd6219498f8a4260e360906fcc71b
yxykk <y1432621251@qq.com>
2022-06-15 19:15:52 +0800

    modify

--- chapter6_示例代码/chapter_6.dart
+++ chapter6_示例代码/chapter_6.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Home'),
+      home: const MyHomePage(title: 'Flutter Home---可滚动组件'),
     );
   }
 }
